7fac6514b664c0fecca9036f4fc59ea7acbdac45
alx-ruiz <edwardruiz784@gmail.com>
2026-03-23 19:31:39 -0500

    Auto-publish: Mon Mar 23 19:31:39 CDT 2026

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
